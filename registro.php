184006e9011274dc932f21702cc87df037e4c074
Lord-bus <raulbus073@gmail.com>
2019-11-27 23:23:36 -0300

    agrego contactos y registro

---
+++ registro.php
@@ -0,0 +1,69 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		
+		<div>
+			<header>
+				<nav class="navbar navbar-default">
+				  <div class="container">
+				    <div class="navbar-header">
+				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				      <a class="navbar-brand" href="index.php">Sitio</a>
+				    </div>
+				  </div>
+				</nav>
+			</header>
+			
+			<section class="container-fluid">
+				<div class="row">
+					<div class="col-md-4"></div>
+					<div class="col-md-4">
+						<form>
+							<h4>Registro de Usuario:</h4>
+							<div class="form-group">
+								<label for="name">Nombre</label>
+								<input type="text" class="form-control" placeholder="Nombre">
+							</div>
+							<div class="form-group">
+								<label for="name">Apellido</label>
+								<input type="text" class="form-control" placeholder="Apellido">
+							</div>
+							<div class="form-group">
+								<label for="name">Teléfono</label>
+								<input type="number" class="form-control" placeholder="Teléfono">
+							</div>
+							<div class="form-group">
+								<label for="email">Email</label>
+								<input type="email" class="form-control" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" placeholder="Password">
+							</div>
+							<button type="submit" class="btn btn-default">Registrarse</button>
+						</form>
+					</div>
+					<div class="col-md-4"></div>
+				</div>
+			</section>
+		</div>
+		
+		<script
+			  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+			  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+			  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</body>
+</html>
